cc75e3f9122aa7d6ae24138c1ec3a9ceb3c8d8e5
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-11-05 01:22:55 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,17 @@ const client = new Discord.Client();
 
 
 client.login('NTA4Njg1MDgwNDc5MjY4OTAz.DsC1rQ.r_Kieyh7ojnTcR6Ua2fF5kLgEvc');
-
+let prefix = "p ";
 client.on('message', async message => {
   // Voice only works in guilds, if the message does not come from a guild,
   // we ignore it
   if (!message.guild) return;
+  
+  if (message.content.startsWith(prefix + "youtube")) {
+    message.channel.send("https://www.youtube.com/channel/UC4KN0anLirlTsn95Ugrroeg");
+  }
 
-  if (message.content === '/join') {
+  if (message.content === 'join') {
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join();
